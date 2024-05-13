e54875098d97b6b51514c780abc1f9c5ef2958a0
Manas <iamscientistmanas@gmail.com>
2024-05-13 12:08:00 +0530

    T-1 assign

---
+++ bot.py
@@ -0,0 +1,3 @@
+#TODO-1: import the discord.py and os, load env and try accessing getting the message reads by the bot
+
+#TODO-2: follow the tutuial in youtube and in solution to get it work, use need to complete till app.py till L-1 (update) commit, you can take help of solution initially but do whatever u want gemini, gpt, but undersatnd how it is working.
